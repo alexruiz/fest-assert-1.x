e665cec9a467de46f824f811182d4ee4ea6a04e4
Alex Ruiz <alex.ruiz.05@gmail.com>
2011-01-12 07:04:12 -0800

    Recovering tests that were accidentally removed while fixing FEST-413.

---
+++ src/test/java/org/fest/assertions/GenericAssert_TestCase.java
@@ -0,0 +1,32 @@
+/*
+ * Created on Jun 17, 2010
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright @2010 the original author or authors.
+ */
+package org.fest.assertions;
+
+/**
+ * Base class for testing implementations of <code>{@link GenericAssert}</code>.
+ * @param <S> used to simulate "self types." For more information please read &quot;<a
+ * href="http://passion.forco.de/content/emulating-self-types-using-java-generics-simplify-fluent-api-implementation"
+ * target="_blank">Emulating 'self types' using Java Generics to simplify fluent API implementation</a>.&quot;
+ * @param <A> The type supported by the implementation of the {@code GenericAssert} to test.
+ *
+ * @author Alex Ruiz
+ */
+public abstract class GenericAssert_TestCase<S extends GenericAssert<S, A>, A> {
+
+  protected abstract A notNullValue();
+
+  protected abstract GenericAssert<S, A> assertionsFor(A actual);
+}
